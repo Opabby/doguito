5b3e67cbaecaba9a99f191900963e4b8979ccbd8
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-05-12 10:53:45 -0300

    Customizing error messages

--- js/validacao.js
+++ js/validacao.js
@@ -7,10 +7,19 @@ export function valida(input) {
 
     if(input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
     } else {
         input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro (tipoDeInput, input)
     }
 }
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
 
 const mensagensDeErro = {
     nome: {
@@ -35,20 +44,31 @@ const validadores = {
     dataNascimento:input => validaDataNascimento(input)
 }
 
+function mostraMensagemDeErro(tipoDeInput, input){
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagem
+}
+
 function validaDataNascimento(input){
-    const dataRecebida = new Date(input.value);
-    let mensagem = '';
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
 
     if(!maiorQue18(dataRecebida)) {
-        mensagem = 'Você deve ser maior que 18 anos para se cadastrar';
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar'
     }
     
-    input.setCustomValidity(mensagem);
+    input.setCustomValidity(mensagem)
 }
 
 function maiorQue18(data){
-    const dataAtual = new Date();
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
-    return dataMais18 <= dataAtual;
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+    return dataMais18 <= dataAtual
 
 }
